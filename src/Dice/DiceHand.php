871f74aeae37400aafbcaa9cbeda79d040af7765
Robert Israelsson <israelsson.r@gmail.com>
2021-03-31 12:59:00 +0200

    Added DiceHand that rolls a number of of Dices.

---
+++ src/Dice/DiceHand.php
@@ -0,0 +1,66 @@
+<?php
+namespace Rois\Dice;
+
+class DiceHand {
+    /**
+     * @var dices - Array containing dices.
+     * @var values - Array containing dice roll values.
+     */
+    private $dices;
+    private $values;
+    
+
+    /**
+     * Constructor to initialize DeceHand with a number of Dices.
+     * @param int $dices - Number of dices.
+     */
+    public function __construct(int $dices = 5) {
+        $this->dices = [];
+        $this->values = [];
+
+        for ($i = 0; $i < $dices; $i++) {
+            $this->dices[$i] = new Dice();
+            $this->values[$i] = null;
+        }
+    }
+
+    /**
+     * Rolls all dices in the hand and save values.
+     * @return void
+     */
+    public function roll() :void
+    {
+        for ($i = 0; $i < count($this->dices); $i++) {
+            $this->dices[$i]->roll();
+            $this->values[$i] = $this->dices[$i]->getLastRoll();
+        }
+    }
+
+    /**
+     * Get the values from all dices from last roll.
+     * @return array - Array of resulting dice values from last roll.
+     */
+    public function values() :array
+    {
+        return $this->values;
+    }
+
+    /**
+     * Calculate the sum of all values from last roll.
+     * @return int - The sum of all dice values from last roll.
+     */
+    public function sum() :int
+    {
+        return array_sum($this->values);
+    }
+
+    /**
+     * Calculates the average of all dice values from last roll.
+     * @return float - The average of all dice values from last roll.
+     */
+    public function average() :float
+    {
+        return array_sum($this->values) / count($this->values);
+    }
+
+}
--- view/layout/dice.php
+++ view/layout/dice.php
@@ -9,10 +9,16 @@ declare(strict_types=1);
 $header = $header ?? null;
 $message = $message ?? null;
 
-$dice = new \Rois\Dice\Dice();
-$dice->roll();
+$diceHand = new \Rois\Dice\DiceHand();
+$diceHand->roll();
+
+$diceValues = $diceHand->values();
 
 ?><h1><?= $header ?></h1>
 
 <p><?= $message ?></p>
-<p><?= $dice->getLastRoll() ?></p>
+<?php for($i = 0; $i < count($diceValues); $i++): ?>
+    <p># <?= $i + 1 ?> -> <?= $diceValues[$i] ?></p>
+<?php endfor; ?>
+<p>Sum: <?= $diceHand->sum() ?></p>
+<p>Average: <?= $diceHand->average() ?></p>
