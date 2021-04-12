87cc60271a6ef7bb0c2986fafdbe1d3e62629020
Robert Israelsson <israelsson.r@gmail.com>
2021-04-12 11:26:31 +0200

    Added pseudocode and flowchart

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -3,12 +3,7 @@
 
     for (let i = 0; i < rolledDices.children.length; i++) {
         rolledDices[i].addEventListener("click", function() {
-            fetch("/dice/game", {
-                method: "POST", 
-                body: JSON.stringify(data)
-              }).then(res => {
-                console.log("Request complete! response:", res);
-              });
+            
         }); 
     }
 })();
